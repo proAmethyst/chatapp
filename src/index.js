7523543c7f9ca0abb96b59c6390be433b7395412
Profir Mihai <104903736+proAmethyst@users.noreply.github.com>
2023-04-03 16:15:39 +0300

    new change

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter as Router } from 'react-router-dom';
 import App from './App';
 import { AuthContextProvider } from './context/AuthContext';
 import { ChatContextProvider } from './context/ChatContext';
@@ -10,9 +9,7 @@ root.render(
   <AuthContextProvider>
     <ChatContextProvider>
     <React.StrictMode>
-      <Router>
         <App />
-      </Router>
     </React.StrictMode>
     </ChatContextProvider>
   </AuthContextProvider>
