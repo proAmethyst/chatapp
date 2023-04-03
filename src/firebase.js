2799fdcef8838d221ce941f6b7b25c9cd0d18a9d
Profir Mihai <104903736+proAmethyst@users.noreply.github.com>
2023-04-03 16:24:02 +0300

    new change

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCFEmGThzdX2cv3HoPOlJPMC0V9GKD7tio",
-  authDomain: "chat-app-44902.firebaseapp.com",
-  projectId: "chat-app-44902",
-  storageBucket: "chat-app-44902.appspot.com",
-  messagingSenderId: "740447430483",
-  appId: "1:740447430483:web:aebd596d4abd8cc1fba3a6"
+  apiKey: "...",
+  authDomain: "...",
+  projectId: "...",
+  storageBucket: "...",
+  messagingSenderId: "...",
+  appId: "..."
 };
 
 // Initialize Firebase
